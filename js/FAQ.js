a941b8610b738bd72d26e5981960f32aef39a3a4
LamVanDuc <88825052+LamVanDuc@users.noreply.github.com>
2022-02-07 20:47:17 +0700

    lamduc-07/02/2022

--- js/FAQ.js
+++ js/FAQ.js
@@ -3,7 +3,6 @@ $(document).ready(function (){
     $('.content .row .col-md-8 .answer').hide();
     $('.content .row .col-md-8 button').click(function (){
         var a_href = $(this).children('a').attr('href');
-            $(a_href).toggle();
-
+             $(a_href).toggle();
     })
 })
