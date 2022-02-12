44fbda8df5813e3ef8c29188299316764ebfb266
LamVanDuc <88825052+LamVanDuc@users.noreply.github.com>
2022-02-12 23:16:19 +0700

    lamduc-12/02/2022

---
+++ editor/js/home_post.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    setTimeout(function () {
+        var max = 100;
+        var tot, str;
+        $('.noiDung').each(function () {
+            str = String($(this).html());
+            tot = str.length;
+            str = (tot <= max) ? str : (str.substring(0, (max + 1))) + "...";
+            $(this).html(str);
+        })
+    }, 500);
+})
