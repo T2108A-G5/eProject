5d003f32f1a32b25d6cd48bb9245df1115a37bce
HiepTrinh_ <88824625+Cumlord22@users.noreply.github.com>
2022-03-26 13:32:08 +0700

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,10 @@
+package authentication;
+
+public class Application {
+    public static void main(String[] args) throws Exception {
+
+        Controller controller = new Controller();
+        controller.home();
+    }
+}
+
